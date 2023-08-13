1cd69b45161810e60aea557607d3115dbe059bab
VladZorgan <ponomarev.vs@pobedafinance.ru>
2023-08-13 10:24:49 +0300

    fix

---
+++ api/projects/type/web-developer-site/modules/services/migrations/create_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 255)->comment('Название услуги');
+            $table->string('slug', 255)->comment('Уникальный текстовый идентификатор');
+            $table->text('description')->comment('Описание услуги');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
